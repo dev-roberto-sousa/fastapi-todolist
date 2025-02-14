a2db21eb00e3055a3026f8d6562167df11fde2cb
dev.roberto.sousa <dev.roberto.sousa@gmail.com>
2025-02-14 10:28:09 -0300

    Updating endpoints

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,6 @@
-from dataclasses import asdict
 from http import HTTPStatus
 
-from sqlalchemy import select
-
-from fast_todo.models import User
+from fast_todo.schemas import UserPublic
 
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
@@ -13,18 +10,80 @@ def test_root_deve_retornar_ok_e_ola_mundo(client):
     assert response.json() == {'message': 'Hello!'}
 
 
-def test_create_user(session, mock_db_time):
-    with mock_db_time(model=User) as time:
-        new_user = User(username='juba', password='secret', email='teste@test')
-        session.add(new_user)
-        session.commit()
-
-    user = session.scalar(select(User).where(User.username == 'juba'))
-
-    assert asdict(user) == {
-        'id': 1,
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'juba',
+            'email': 'teste@test.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
         'username': 'juba',
-        'password': 'secret',
-        'email': 'teste@test',
-        'created_at': time,
+        'email': 'teste@test.com',
+        'id': 1,
     }
+
+
+def test_read_users(client):
+    response = client.get('/users')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': []}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'beto',
+            'email': 'beto@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'beto',
+        'email': 'beto@example.com',
+        'id': 1,
+    }
+
+
+def test_update_integrity_error(client, user):
+    # Inserindo fausto
+    client.post(
+        '/users',
+        json={
+            'username': 'fausto',
+            'email': 'fausto@example.com',
+            'password': 'secret',
+        },
+    )
+
+    # Alterando o user das fixture para fausto
+    response_update = client.put(
+        f'/users/{user.id}',
+        json={
+            'username': 'fausto',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    assert response_update.status_code == HTTPStatus.CONFLICT
+    assert response_update.json() == {
+        'detail': 'Username or Email already exists'
+    }
+
+
+def test_delete_user(client, user):
+    response = client.delete('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
--- tests/test_db.py
+++ tests/test_db.py
@@ -7,9 +7,7 @@ from fast_todo.models import User
 
 def test_create_user(session, mock_db_time):
     with mock_db_time(model=User) as time:
-        new_user = User(
-            username='juba', password='secret', email='teste@test'
-        )
+        new_user = User(username='juba', password='secret', email='teste@test')
         session.add(new_user)
         session.commit()
 
